48efc1d78a7658b938cfd3f951a3483e1f811324
Shishupal Singh <shishupalsingh1137@gmail.com>
2026-04-13 21:45:32 +0530

    initial selenium project

---
+++ src/test/java/com/test/seleniumproject/Test1.java
@@ -0,0 +1,23 @@
+package com.test.seleniumproject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test1 {
+
+    @Test
+    public void openGoogle() {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+
+        System.out.println("Title: " + driver.getTitle());
+
+        driver.quit();
+    }
+}
