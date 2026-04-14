40e3830db6d5f535bfdc491595c58128b0c58b4b
Shishupal Singh <shishupalsingh1137@gmail.com>
2026-04-14 11:40:27 +0530

    added wrong code

--- src/test/java/com/test/seleniumproject/Test1.java
+++ src/test/java/com/test/seleniumproject/Test1.java
@@ -19,6 +19,8 @@ public class Test1 {
         System.out.println("Title: " + driver.getTitle());
         
         System.out.println("PR demo working");
+        
+        System.out.println("WRONG CODE");
 
         driver.quit();
     }
